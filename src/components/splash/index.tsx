34d89f9f51bf1afc5fdc9b5ded07533bcd3b03d7
David Osaro <90871488+davidosaro@users.noreply.github.com>
2023-06-24 00:14:24 +0100

    setup

---
+++ src/components/splash/index.tsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+
+function Splash() {
+  return (
+    <div>
+      <section className='w-full absolute inset-0 bg-black text-white grid place-items-center'>
+        <div className='font-montserrat font-semibold text-[48px]'>Floo</div>
+      </section>
+    </div>
+  );
+}
+
+export default Splash;
---
+++ src/pages/landing/index.tsx
@@ -0,0 +1,15 @@
+import React, { useEffect } from 'react';
+
+function Landing() {
+  return (
+    <div>
+      <section className='w-full h-screen bg-black text-white flex justify-center'>
+        <div className='font-montserrat font-semibold text-[56px] mt-[160px]'>Floo</div>
+      </section>
+      <section className='w-full h-[200px]'></section>
+
+    </div>
+  );
+}
+
+export default Landing;
